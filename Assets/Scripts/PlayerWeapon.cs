7e5bbaf211ab1594c450e24f808f3057e3c4caf7
slawdawg <axeshock@gmail.com>
2019-01-02 17:23:56 -0600

    created player weapon class

---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+
+
+    public class PlayerWeapon : MonoBehaviour
+    {
+        public string name = "Weapon Name 1";
+
+        public float damage = 10f;
+        public float range = 100f;
+
+
+    }
+
+
